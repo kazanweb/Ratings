bdf74912a1339ca409bf4268a225bd667c59ddf4
linedotwww <linedot.www@gmail.com>
2017-10-23 11:30:18 +0300

    sdf

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 $(document).ready(function() {
 
-	var rates = document.querySelectorAll('.js-ratings__section');
-
-	Array.prototype.forEach.call(rates, function(item, index) {
-
-		new Ratings({
-			element: item,
-			width: 13 * 5 / 5,
-			clickFn: function(index) {
-				alert(index);
-			}
-		});
-
+	new Ratings({
+		element: document.querySelector('.js-ratings__section'),
+		width: 120 / 5,
+		clickFn: function(index) {
+			alert(index);
+		}
 	});
 
 });
